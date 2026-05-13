e5639f2d3a56261562c5383a38a282fd7dd026e2
vivek-d24 <vivekd0dev@gmail.com>
2026-05-13 16:10:47 +0530

    Fix Firebase config for deployment

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,13 +1,30 @@
 import { getApp, getApps, initializeApp, type FirebaseApp } from "firebase/app";
 
+const fallbackFirebaseConfig = {
+  apiKey: "AIzaSyAV8kdwGQ5tUZwCMunKuwfPuciPFwPC1q0",
+  authDomain: "fourth-sem-qp.firebaseapp.com",
+  projectId: "fourth-sem-qp",
+  storageBucket: "fourth-sem-qp.firebasestorage.app",
+  messagingSenderId: "433997046896",
+  appId: "1:433997046896:web:f08149391d93ff87e200b5",
+};
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "",
+  apiKey:
+    process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? fallbackFirebaseConfig.apiKey,
+  authDomain:
+    process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ??
+    fallbackFirebaseConfig.authDomain,
+  projectId:
+    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ??
+    fallbackFirebaseConfig.projectId,
+  storageBucket:
+    process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ??
+    fallbackFirebaseConfig.storageBucket,
   messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? "",
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ??
+    fallbackFirebaseConfig.messagingSenderId,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? fallbackFirebaseConfig.appId,
 };
 
 export const isFirebaseConfigured = Object.values(firebaseConfig).every(Boolean);
